d6046f993883e82728c374f400ef9997d9ced695
Emilijus <slibinas2012@gmail.com>
2018-09-07 17:13:47 +0300

    Added editor snapping settings

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [Range(1f, 20f)] [SerializeField] float gridSize = 10;
+
+    TextMesh textMesh;
+
+    void Awake()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
+
+    void Update ()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        textMesh.text = snapPos.x / gridSize + "." + snapPos.z / gridSize;
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+    }
+}
