01165795ad4a5baadb29176046c9af7dbf81cc0c
Emilijus <slibinas2012@gmail.com>
2018-09-09 14:07:55 +0300

    Enemy moving in a grid

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+    
+	void Start ()
+    {
+        StartCoroutine(FollowPath());
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    IEnumerator FollowPath()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSecondsRealtime(1f);
+        }
+    }
+}
